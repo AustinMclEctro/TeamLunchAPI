e6420d2c58cdb25cc581c4ab2c8faf36fb0f1308
Austin Sullivan <austinmclastro@gmail.com>
2018-03-04 17:56:50 -0700

    Initial commit

---
+++ TeamLunch UnitTests/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using TeamLunchAPI.Controllers;
+
+namespace TeamLunch_UnitTests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void GetMealsTest()
+        {
+            HomeController controller = new HomeController();
+            var result = controller.GetMeals() as Dictionary<KeyValuePair<string, string>, string>;
+            //var a = result.
+        }
+    }
+}
---
+++ TeamLunch UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TeamLunch UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TeamLunch UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30ec7152-731d-4ba6-bbfe-6012bc478dc9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
